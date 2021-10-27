7707798c25dfcf76dfdd7683945ff83bca2d197e
yliwang <1220264221@qq.com>
2021-10-27 16:53:42 +0800

    Add files via upload

---
+++ autolevel.py
@@ -0,0 +1,27 @@
+# python implementation of image autolevel.
+
+
+def GenAutolevelMapping( hist ): #hist is the histogram of a 8 bit gray scale image
+
+
+
+
+
+
+def ImageAutolevel( f ) # f is the input image, as Image object or numpy array
+
+
+
+
+
+    return g #g is the auto-level-processed image
+
+
+def Main():
+
+
+
+
+
+if __name__=='__main__':
+    Main()
